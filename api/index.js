59caebd105e7947e84c3243825b81dab179548c3
jeisson333 <ljeiisonl@gmail.com>
2023-10-03 11:54:52 -0500

    Subir proyecto

---
+++ api/index.js
@@ -0,0 +1,17 @@
+// H   H  AAAAA  PPPPP   PPPPP  IIIII EEEEE  RRRRR
+// H   H  A   A  P   P   P   P   I    E      R   R
+// HHHHH  AAAAA  PPPPP   PPPPP   I    EEEE   RRRRR
+// H   H  A   A  P       P       I    E      R  R
+// H   H  A   A  P       P     IIIII  EEEEE  R   R
+
+// // //~~~~~~~~~~~~By:Jo~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const PORT = 3001;
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server raised in port: ${PORT}`); // eslint-disable-line no-console
+  });
+});
